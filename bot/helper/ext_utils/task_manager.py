579f3e2f069c8fb415236cb5d59c0ec6df2deec7
zeyn89 <128371500+ZeynDev@users.noreply.github.com>
2023-11-17 20:31:53 +0700

    Test

--- bot/helper/ext_utils/task_manager.py
+++ bot/helper/ext_utils/task_manager.py
@@ -22,7 +22,9 @@ async def stop_duplicate_check(listener):
         or listener.select
         or not is_gdrive_id(listener.upDest)
         or listener.upDest.startswith("mtp:")
-        and listener.stopDuplicate
+        and not listener.user_dict.get('stop_duplicate', False) 
+        or config_dict['STOP_DUPLICATE']
+        and listener.upDest.startswith('mtp:')
         or not listener.stopDuplicate
         or listener.sameDir
     ):
