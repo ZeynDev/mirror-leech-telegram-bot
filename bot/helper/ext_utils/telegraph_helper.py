48ba945419d9508dfc8814e175ab0d3d200914d6
ZeynXD <128371500+ZeynDev@users.noreply.github.com>
2024-12-14 07:17:39 +0700

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -73,14 +73,14 @@ class TelegraphHelper:
                     nxt_page += 1
             await self.edit_page(
                 path=path[prev_page],
-                title="Mirror-leech-bot Torrent Search",
+                title="Nayo Search",
                 content=content,
             )
         return
 
 
 telegraph = TelegraphHelper(
-    "Mirror-Leech-Telegram-Bot", "https://github.com/anasty17/mirror-leech-telegram-bot"
+    "Nayo Genk", "https://t.me/NayoGN"
 )
 
 print(__name__)
